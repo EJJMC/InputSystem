8bb694f468c833fe08cc54088901a5ea3ecb6f31
Eoin McSharry <eoinjmcsharry@gmail.com>
2023-08-23 14:30:45 +0100

    flipped

---
+++ src/BottomNavbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BottomNavbar.css';
+
+const BottomNavbar = () => {
+  return (
+    <div className="BottomNavbar">
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+    </div>
+  );
+};
+
+export default BottomNavbar;
